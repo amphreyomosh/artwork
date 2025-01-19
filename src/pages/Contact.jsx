71b9199673ffa83a7124b0526df3340b619a81f0
amphreyomosh <amphreyomosh2001@gmail.com>
2025-01-19 14:05:46 +0300

    made some changes on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -37,10 +37,10 @@ const Contact = () => {
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="mx-auto max-w-2xl text-center">
         <h2 className="text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-          Contact Sales
+          Contact Me!
         </h2>
         <p className="mt-2 text-lg text-gray-600">
-          Aute magna irure deserunt veniam aliqua magna enim voluptate.
+          I will create one your favourite art work.
         </p>
       </div>
       <form
