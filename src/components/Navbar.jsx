32276bb41aaa63749cebaf1304d1dd4f1ff09c3d
amphreyomosh <amphreyomosh2001@gmail.com>
2025-01-20 18:25:34 +0300

    made changes on the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,64 +59,134 @@ function Navbar() {
 
     // Clear search input after searching
     setSearchQuery('');
+
+    // Close the menu when the search button is clicked on small screens
+    if (isOpen) {
+      setIsOpen(false);
+    }
   };
 
   return (
-    <nav className="fixed top-0 left-0 w-full bg-white text-white p-5 z-50">
-      <div className="flex justify-between items-center w-full">
+    <nav className='fixed top-0 left-0 w-full bg-white text-white p-5 z-50'>
+      <div className='flex justify-between items-center w-full'>
         {/* Logo on the far left */}
-        <div className="text-2xl font-bold">
+        <div className='text-2xl font-bold'>
           <img
-            src="/jopelogo.png"
-            alt="jopegraphics logo"
+            src='/jopelogo.png'
+            alt='jopegraphics logo'
             style={{
               width: '70px',
               height: '50px',
               borderRadius: '10%',
               imageRendering: 'auto',
               filter: 'brightness(1)',
+              zIndex: '10000',
             }}
           />
         </div>
 
         {/* Hamburger menu for small screens */}
-        <div className="md:hidden text-black">
-          <button onClick={() => setIsOpen(!isOpen)} className="text-4xl">
+        <div className='md:hidden text-black'>
+          <button onClick={() => setIsOpen(!isOpen)} className='text-4xl'>
             &#9776;
           </button>
         </div>
 
         {/* Navigation links for large screens */}
-        <div className="hidden md:flex space-x-7 ml-auto">
-          <Link to="/" className="hover:text-yellow-300 text-black">
+        <div className='hidden md:flex space-x-7 ml-auto'>
+          <Link to='/' className='hover:text-yellow-300 text-black'>
             Home
           </Link>
-          <Link to="/about" className="hover:text-yellow-300 text-black">
+          <Link to='/about' className='hover:text-yellow-300 text-black'>
             About
           </Link>
-          <Link to="/projects" className="hover:text-yellow-300 text-black">
+          <Link to='/projects' className='hover:text-yellow-300 text-black'>
             Projects
           </Link>
-          <Link to="/contact" className="hover:text-yellow-300 text-black">
+          <Link to='/contact' className='hover:text-yellow-300 text-black'>
             Contact
           </Link>
         </div>
 
-        {/* Search Bar */}
-        <div className="hidden md:flex ml-4 items-center">
-          <div className="flex items-center bg-white rounded-full px-3 py-1 shadow-md">
-            <i className="text-black text-lg">&#128269;</i>
+        {/* Search Bar on Large Screens */}
+        <div className='hidden md:flex ml-4 items-center'>
+          <div className='flex items-center bg-white rounded-full px-3 py-1 shadow-md'>
+            <i className='text-black text-lg'>&#128269;</i>
             <input
-              type="text"
-              placeholder="Search for art..."
+              type='text'
+              placeholder='Search for art...'
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
-              className="ml-2 px-4 py-2 rounded-full bg-transparent text-gray-700 focus:outline-none w-0 transition-all ease-in-out duration-300 focus:w-56"
+              className='ml-2 px-4 py-2 rounded-full bg-transparent text-gray-700 focus:outline-none w-0 transition-all ease-in-out duration-300 focus:w-56'
             />
           </div>
           <button
             onClick={handleSearch}
-            className="ml-2 px-4 py-2 bg-yellow-500 text-white rounded-full hover:bg-yellow-600"
+            className='ml-2 px-4 py-2 bg-yellow-500 text-white rounded-full hover:bg-yellow-600'
+          >
+            Search
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile Navigation Links with Curtain Effect */}
+      <div
+        className={`md:hidden transition-all duration-700 ease-in-out ${isOpen ? 'scale-x-100 translate-x-0' : 'scale-x-0 translate-x-full'} absolute top-0 left-0 w-full bg-white p-5 h-screen`}
+        style={{
+          transformOrigin: 'left',
+        }}
+      >
+        <div className='flex justify-between items-center w-full'>
+          <div className='text-2xl font-bold'>
+            <img
+              src='/jopelogo.png'
+              alt='jopegraphics logo'
+              style={{
+                width: '70px',
+                height: '50px',
+                borderRadius: '10%',
+                imageRendering: 'auto',
+                filter: 'brightness(1)',
+                zIndex: '10000',
+              }}
+            />
+          </div>
+          <div className='md:hidden text-black'>
+            <button onClick={() => setIsOpen(!isOpen)} className='text-4xl'>
+              &#9776;
+            </button>
+          </div>
+        </div>
+        <div className='flex flex-col items-end space-y-4 py-4'>
+          <Link to='/' className='text-black hover:text-yellow-300' onClick={() => setIsOpen(false)}>
+            Home
+          </Link>
+          <Link to='/about' className='text-black hover:text-yellow-300' onClick={() => setIsOpen(false)}>
+            About
+          </Link>
+          <Link to='/projects' className='text-black hover:text-yellow-300' onClick={() => setIsOpen(false)}>
+            Projects
+          </Link>
+          <Link to='/contact' className='text-black hover:text-yellow-300' onClick={() => setIsOpen(false)}>
+            Contact
+          </Link>
+        </div>
+
+        {/* Search Bar on Mobile (Search button below input) */}
+        <div className='flex flex-col items-end ml-4 mt-4'>
+          <div className='flex items-center bg-white rounded-full px-3 py-1 shadow-md'>
+            <i className='text-black text-lg'>&#128269;</i>
+            <input
+              type='text'
+              placeholder='Search for art...'
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className='ml-2 px-4 py-2 rounded-full bg-transparent text-gray-700 focus:outline-none w-0 transition-all ease-in-out duration-300 focus:w-56'
+            />
+          </div>
+          <button
+            onClick={handleSearch}
+            className='mt-2 px-4 py-2 bg-yellow-500 text-white rounded-full hover:bg-yellow-600'
           >
             Search
           </button>
